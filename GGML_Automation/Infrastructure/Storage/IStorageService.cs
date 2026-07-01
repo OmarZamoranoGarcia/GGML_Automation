8e37e1a2be71cd12a5013188083fb39869ac6ca2
OmarZamoranoGarcia <omarchucho13Zago0413@hotmail.com>
2026-07-01 16:48:44 -0700

    SupabaseService

---
+++ GGML_Automation/Infrastructure/Storage/IStorageService.cs
@@ -0,0 +1,7 @@
+﻿namespace GGML_Automation.Infrastructure.Storage
+{
+    public interface IStorageService
+    {
+        Task<string> UploadFile(string fileName, byte[] file);
+    }
+}
---
+++ GGML_Automation/Infrastructure/Storage/SupabaseStorageService.cs
@@ -0,0 +1,35 @@
+﻿using Supabase.Storage;
+
+namespace GGML_Automation.Infrastructure.Storage;
+
+public class SupabaseStorageService : IStorageService
+{
+    private readonly Supabase.Client _client;
+
+    public SupabaseStorageService(
+        Supabase.Client client)
+    {
+        _client = client;
+    }
+
+    public async Task<string> UploadFile(
+        string fileName,
+        byte[] file)
+    {
+
+        var bucket =
+            _client.Storage
+            .From("excel-files");
+
+        // Crear nombre único
+        var uniqueName =
+            $"{Guid.NewGuid()}_{fileName}";
+
+        await bucket.Upload(
+            file,
+            uniqueName
+        );
+
+        return uniqueName;
+    }
+}
