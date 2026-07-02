f3fb9e743fc5ec44991058a3d78ef523bc3ba711
OmarZamoranoGarcia <omarchucho13Zago0413@hotmail.com>
2026-07-02 16:38:21 -0700

    WIP: UploadResult

---
+++ GGML_Automation/Infrastructure/Storage/UploadResult.cs
@@ -0,0 +1,8 @@
+﻿namespace GGML_Automation.Infrastructure.Storage;
+
+public class UploadResult
+{
+    public string StoredName { get; set; } = "";
+
+    public string StoragePath { get; set; } = "";
+}
